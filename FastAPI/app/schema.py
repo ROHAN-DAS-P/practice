e80e2ead2f722fa5ceff88697c87e8298b1cf38d
ROHAN DAS P <rohan2003710@gmail.com>
2026-07-24 16:04:12 +0530

    created structure for the response and introduced hashing

---
+++ FastAPI/app/schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+from pydantic.types import conint
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
